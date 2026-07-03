ffa1e6fd986243a1a400932d9a3fbf3fa071d294
David <lazic.ddavid@gmail.com>
2026-07-03 22:20:23 +0100

    update JS button messages

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showMessage() {
     const output = document.getElementById('output');
-    const messages = ['Hello from JavaScript!', 'Welcome to Projekat 44!', 'Click again!'];
+    const messages = ['Pozdrav!', 'Dobrodosao!', 'Klikni opet!'];
     const index = Math.floor(Math.random() * messages.length);
     output.textContent = messages[index];
 }
