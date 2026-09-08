8d5e79debc8dc206951d218ae702af0aebdf8395
David <lazic.ddavid@gmail.com>
2026-09-08 21:46:33 +0200

    update JS button messages

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showMessage() {
     const output = document.getElementById('output');
-    const messages = ['Hello!', 'Welcome!', 'Click again!'];
+    const messages = ['Hi there!', 'Projekat 44', 'Nice click!'];
     const index = Math.floor(Math.random() * messages.length);
     output.textContent = messages[index];
 }
